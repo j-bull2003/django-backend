176541e5f9f60a266c5f66b032e9d9ce52284d16
j-bull2003 <jessicabull833@gmail.com>
2024-06-08 19:47:04 +0100

    foerign key in model

--- backend/users/users/models.py
+++ backend/users/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Occupations(models.Model):
-    job_title = models.CharField(max_length=50, primary_key = True)
+    job_title = models.CharField(max_length=50, primary_key=True)
     job_description = models.CharField(max_length=200)
     
     def __str__(self):
